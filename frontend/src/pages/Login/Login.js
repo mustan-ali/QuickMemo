294fddcd82ae2faeb02922b43b38b5d51eb372ae
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-06-18 17:43:12 +0500

    Refactor code

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,66 +1,96 @@
-import { React, useState } from 'react'
-import { Link } from 'react-router-dom'
-import Navbar from '../../components/Navbar/Navbar'
-import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa'
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [isShowPassword, setIsShowPassword] = useState(false);
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState(null)
-    const [isShowPassword, setIsShowPassword] = useState(false)
+  const toggleShowPassword = () => {
+    setIsShowPassword(!isShowPassword);
+  };
 
-    const toggleShowPassword = () => {
-        setIsShowPassword(!isShowPassword)
+  const validateEmail = (email) => {
+    const regex = /^[[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Enter a valid email address");
+      return;
     }
 
-    const validateEmail = (email) => {
-        const regex = /^[[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return regex.test(email)
+    if (!password) {
+      setError("Enter a password");
+      return;
     }
 
-    const handleLogin = async (e) => {
-        e.preventDefault()
+    setError("");
+  };
 
-        if (!validateEmail(email)) {
-            setError('Enter a valid email address')
-            return
-        }
+  return (
+    <>
+      <Navbar />
 
-        if (!password) {
-            setError('Enter a password')
-            return
-        }
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleLogin}>
+            <h4 className="text-2xl mb-7">Login</h4>
 
-        setError("")
-    }
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              className="input-box"
+            />
 
-    return (
-        <>
-            <Navbar />
+            <div className="flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3">
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type={isShowPassword ? "text" : "password"}
+                placeholder="Password"
+                className="w-full text-sm bg-transparent py-3 mr-3 rounded outline-none"
+              />
 
-            <div className="flex items-center justify-center mt-28">
-                <div className="w-96 border rounded bg-white px-7 py-10">
-                    <form onSubmit={handleLogin}>
-                        <h4 className="text-2xl mb-7">Login</h4>
-
-                        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' className="input-box" />
-
-                        <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3'>
-                            <input value={password} onChange={(e) => setPassword(e.target.value)} type={isShowPassword ? 'text' : 'password'} placeholder='Password' className="w-full text-sm bg-transparent py-3 mr-3 rounded outline-none" />
-
-                            {isShowPassword ? <FaRegEye size={22} onClick={toggleShowPassword} className="text-primary cursor-pointer" /> : <FaRegEyeSlash size={22} onClick={toggleShowPassword} className="text-slate-400 cursor-pointer" />}
-                        </div>
-
-                        {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
-                        <button type='submit' className="btn-primary">Login</button>
-
-                        <p className="text-sm text-center mt-4">Not registered yet? {" "}<Link to="/signUp" className="font-medium text-primary underline">Create an account</Link></p>
-                    </form>
-                </div>
+              {isShowPassword ? (
+                <FaRegEye
+                  size={22}
+                  onClick={toggleShowPassword}
+                  className="text-primary cursor-pointer"
+                />
+              ) : (
+                <FaRegEyeSlash
+                  size={22}
+                  onClick={toggleShowPassword}
+                  className="text-slate-400 cursor-pointer"
+                />
+              )}
             </div>
-        </>
-    )
-}
 
-export default Login
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Not registered yet?{" "}
+              <Link to="/signup" className="font-medium text-primary underline">
+                Create an account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- frontend/src/pages/SignUp/SignUp.js
+++ frontend/src/pages/SignUp/SignUp.js
@@ -1,68 +1,104 @@
-import { React, useState } from 'react'
-import { Link } from 'react-router-dom'
-import Navbar from '../../components/Navbar/Navbar'
-import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa'
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [isShowPassword, setIsShowPassword] = useState(false);
 
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState(null)
-    const [isShowPassword, setIsShowPassword] = useState(false)
+  const toggleShowPassword = () => {
+    setIsShowPassword(!isShowPassword);
+  };
 
-    const toggleShowPassword = () => {
-        setIsShowPassword(!isShowPassword)
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setError("Enter your name");
+      return;
     }
 
-    const handleSignUp = async (e) => {
-        e.preventDefault()
-
-        if (!name) {
-            setError('Enter your name')
-            return
-        }
-
-        if (!email) {
-            setError('Enter an email address')
-            return
-        }
-
-        if (!password) {
-            setError('Enter a password')
-            return
-        }
-
-        setError("")
+    if (!email) {
+      setError("Enter an email address");
+      return;
     }
 
-    return (
-        <>
-            <Navbar />
+    if (!password) {
+      setError("Enter a password");
+      return;
+    }
 
-            <div className="flex justify-center items-center mt-28">
-                <div className="w-96 border rounded bg-white px-7 py-10">
-                    <form onSubmit={handleSignUp}>
-                        <h4 className="text-2xl mb-7">Sign Up</h4>
+    setError("");
+  };
 
-                        <input type='text' placeholder='Name' className='input-box' value={name} onChange={(e) => setName(e.target.value)} />
-                        <input type='email' placeholder='Email' className='input-box' value={email} onChange={(e) => setEmail(e.target.value)} />
+  return (
+    <>
+      <Navbar />
 
-                        <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3'>
-                            <input value={password} onChange={(e) => setPassword(e.target.value)} type={isShowPassword ? 'text' : 'password'} placeholder='Password' className="w-full text-sm bg-transparent py-3 mr-3 rounded outline-none" />
+      <div className="flex justify-center items-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl mb-7">Sign Up</h4>
 
-                            {isShowPassword ? <FaRegEye size={22} onClick={toggleShowPassword} className="text-primary cursor-pointer" /> : <FaRegEyeSlash size={22} onClick={toggleShowPassword} className="text-slate-400 cursor-pointer" />}
-                        </div>
+            <input
+              type="text"
+              placeholder="Name"
+              className="input-box"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-                        {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
-                        <button type='submit' className="btn-primary">SignUp</button>
+            <div className="flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3">
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type={isShowPassword ? "text" : "password"}
+                placeholder="Password"
+                className="w-full text-sm bg-transparent py-3 mr-3 rounded outline-none"
+              />
 
-                        <p className="text-sm text-center mt-4">Already have an account? {" "}<Link to="/login" className="font-medium text-primary underline">Login</Link></p>
-                    </form>
-                </div>
+              {isShowPassword ? (
+                <FaRegEye
+                  size={22}
+                  onClick={toggleShowPassword}
+                  className="text-primary cursor-pointer"
+                />
+              ) : (
+                <FaRegEyeSlash
+                  size={22}
+                  onClick={toggleShowPassword}
+                  className="text-slate-400 cursor-pointer"
+                />
+              )}
             </div>
-        </>
-    )
-}
 
-export default SignUp
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            <button type="submit" className="btn-primary">
+              SignUp
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Already have an account?{" "}
+              <Link to="/login" className="font-medium text-primary underline">
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
