e4350264a924619728516925db23e33692165832
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-06-18 15:14:35 +0500

    Add routes for dashboard, login, and signup pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/dashboard" exact element={<Home />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/signup" exact element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
