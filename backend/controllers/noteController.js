1b9483720c228698446fc8c7259234c30cf52b81
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-06-28 00:03:34 -0700

    Add search functionality to fetch notes based on query

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -127,4 +127,27 @@ const pinNote = async (req, res) => {
     }
 }
 
-module.exports = { addNote, editNote, getNotes, deleteNote, pinNote };
+const searchNotes = async (req, res) => {
+    const { user } = req.user;
+    const { query } = req.query;
+
+    if (!query) {
+        return res.status(400).json({ error: true, message: "Query is required" });
+    }
+
+    try {
+        const results = await Note.find({
+            userId: user._id,
+            $or: [
+                { title: { $regex: new RegExp(query, "i") } },
+                { content: { $regex: new RegExp(query, "i") } }
+            ]
+        });
+
+        return res.json({ error: false, notes: results, message: "Notes fetched successfully" });
+
+    } catch (error) {
+        return res.status(500).json({ error: true, message: "Internal server error" });
+    }
+}
+module.exports = { addNote, editNote, getNotes, deleteNote, pinNote, searchNotes };
--- backend/routes/NoteRoute.js
+++ backend/routes/NoteRoute.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const { addNote, editNote, deleteNote, getNotes, pinNote } = require("../controllers/noteController");
+const { addNote, editNote, deleteNote, getNotes, pinNote, searchNotes } = require("../controllers/noteController");
 const authenticateToken = require("../middleware/tokenVerification");
 
-// // Add a new note
+// Add a new note
 router.post("/add-note", authenticateToken, addNote);
 
-// // Edit a note
+// Edit a note
 router.put("/edit-note/:id", authenticateToken, editNote);
 
-// // Delete a note
+// Delete a note
 router.delete("/delete-note/:id", authenticateToken, deleteNote);
 
-// // Get all notes
+// Get all notes
 router.get("/get-notes", authenticateToken, getNotes);
 
-// // Pin a note
+// Pin a note
 router.put("/pin-note/:id", authenticateToken, pinNote);
+
+// Search notes
+router.get("/search-notes", authenticateToken, searchNotes);
 
 module.exports = router;
